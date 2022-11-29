c285840c0ffdbe3440dd4e5ef2e6b66c10cf366f
Manil Agarwal <manil.agarwal21@gmail.com>
2022-11-29 14:14:49 +0530

    firstCommit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -14,10 +15,47 @@ void main() {
     ),
   );
 }
+  
+class DicePage extends StatefulWidget {
 
-class DicePage extends StatelessWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  void changeDiceFace(){
+    setState(() {
+      rightDiceNumber = Random().nextInt(6)+1;
+      leftDiceNumber = Random().nextInt(6)+1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: (){
+                setState(() {
+                  changeDiceFace();
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: (){
+                changeDiceFace();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
